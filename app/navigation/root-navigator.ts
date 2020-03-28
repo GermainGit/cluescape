cf65fec26bd753b36e5a07a1c9c81b0cce4ee051
GermainGit <germain.prevot@gmail.com>
2020-03-28 18:32:29 +0100

    [ADD] Architecture Screen

--- app/navigation/root-navigator.ts
+++ app/navigation/root-navigator.ts
@@ -3,10 +3,32 @@ import { PrimaryNavigator } from "./primary-navigator"
 
 // prettier-ignore
 import {
+  MenuMainScreen,
+  MenuSettingsSettingsScreen,
+  MenuSettingsCguScreen,
+  MenuSettingsNoticeScreen,
+  MenuAccountAccountScreen,
+  MenuAccountSignInScreen,
+  MenuAccountSignUpScreen,
+  GameHomeScreen,
+  GameEnigmaQuizzScreen,
+  GameEndAssemblyScreen,
+  GameEndFinalScreen,
 } from "../screens" // eslint-disable-line @typescript-eslint/no-unused-vars
 
 export const RootNavigator = createStackNavigator(
   {
+    gameEndFinalScreen: { screen: GameEndFinalScreen },
+    gameEndAssemblyScreen: { screen: GameEndAssemblyScreen },
+    gameEnigmaQuizzScreen: { screen: GameEnigmaQuizzScreen },
+    gameHomeScreen: { screen: GameHomeScreen },
+    menuAccountSignUpScreen: { screen: MenuAccountSignUpScreen },
+    menuAccountSignInScreen: { screen: MenuAccountSignInScreen },
+    menuAccountAccountScreen: { screen: MenuAccountAccountScreen },
+    menuSettingsNoticeScreen: { screen: MenuSettingsNoticeScreen },
+    menuSettingsCguScreen: { screen: MenuSettingsCguScreen },
+    menuSettingsSettingsScreen: { screen: MenuSettingsSettingsScreen },
+    menuMainScreen: { screen: MenuMainScreen },
     primaryStack: { screen: PrimaryNavigator },
   },
   {
