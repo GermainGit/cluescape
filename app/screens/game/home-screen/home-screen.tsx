378e4f34e7b0fd1afe2041dc095b70e7c1043af3
Josselin Luneau <josselinluneau@gmail.com>
2020-04-07 12:12:17 +0200

    [WIP] action trigger and leave modal

--- app/screens/game/home-screen/home-screen.tsx
+++ app/screens/game/home-screen/home-screen.tsx
@@ -1,9 +1,11 @@
 import * as React from "react"
+import { useState } from "react"
 import { observer } from "mobx-react-lite"
-import { Image, ImageStyle, TextStyle, View, ViewStyle } from "react-native"
+import { Alert, Image, ImageStyle, Modal, TextStyle, View, ViewStyle } from "react-native"
 import { Button, Screen, Text } from "../../../components"
 import { color } from "../../../theme"
 import { NavigationScreenProp } from "react-navigation"
+import { TouchableNativeFeedback } from "react-native-gesture-handler"
 
 export interface GameHomeScreenProps {
   navigation: NavigationScreenProp<{}>
@@ -54,14 +56,18 @@ const ScannerImageView: ViewStyle = {
 
 const ScannerImage: ImageStyle = {
   resizeMode: "contain",
-  height: 224,
-  width: 224,
+  height: 250,
+  width: 250,
 }
 
 const ScannerMiddleView: ViewStyle = {
+  position: "absolute",
   borderRadius: 50,
   height: 24,
   width: 24,
+
+  borderWidth: 4,
+  borderColor: "rgba(247, 248, 233, 0.68)",
 
   backgroundColor: "#F7F8E9",
 }
@@ -77,6 +83,59 @@ const InventoryView: ViewStyle = {
   backgroundColor: "#1F2F33",
 }
 
+const ModalContainerView: ViewStyle = {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+}
+
+const ModalView: ViewStyle = {
+  justifyContent: "center",
+  alignItems: "center",
+  width: 345,
+  height: 148,
+  alignSelf: "center",
+  backgroundColor: "#83949B",
+  borderRadius: 10,
+}
+
+const ModalButtonView: ViewStyle = {
+  marginTop: 20,
+  width: 200,
+  flexDirection: "row",
+  justifyContent: "space-between",
+}
+
+const ModalTitleText: TextStyle = {
+  fontSize: 24,
+}
+
+const ModalButtonStyle: ViewStyle = {
+  borderRadius: 20,
+  width: 92,
+  height: 42,
+}
+
+const ModalButtonText: TextStyle = {
+  fontSize: 19,
+}
+
+const ModalButtonFull: ViewStyle = {
+  backgroundColor: "#F7F8E9",
+  borderWidth: 1,
+  borderColor: "#F7F8E9",
+}
+
+const ModalButtonFullText: TextStyle = {
+  color: "#83949B",
+}
+
+const ModalButtonBorder: ViewStyle = {
+  backgroundColor: "transparent",
+  borderWidth: 1,
+  borderColor: "#F7F8E9",
+}
+
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
   flex: 1,
@@ -85,21 +144,75 @@ const ROOT: ViewStyle = {
 }
 
 export const GameHomeScreen: React.FunctionComponent<GameHomeScreenProps> = observer((props) => {
+  const leave = React.useMemo(() => () =>
+    props.navigation.navigate("menuMainScreen"), [
+    props.navigation,
+  ])
+
+  const [modalVisible, setModalVisible] = useState(false)
+
+  const openInventory = function() {
+    Alert.alert("Open inventory")
+  }
+
+  const closeGame = function() {
+    setModalVisible(true)
+  }
+
+  const askHelp = function() {
+    Alert.alert("Ask help")
+  }
+
+  const scan = function() {
+    Alert.alert("Scan")
+  }
+
   return (
     <Screen style={ROOT} preset="fixed">
+      <Modal
+        animationType={"fade"}
+        visible={modalVisible}
+        transparent={true}
+      >
+        <View style={ModalContainerView}>
+          <View style={ModalView}>
+            <Text style={ModalTitleText}>Quitter la partie</Text>
+            <View style={ModalButtonView}>
+              <Button
+                style={[ModalButtonStyle, ModalButtonBorder]}
+                textStyle={ModalButtonText}
+                onPress={() => {
+                  setModalVisible(false)
+                  leave()
+                }}
+                text={"Oui"}
+              />
+              <Button
+                style={[ModalButtonStyle, ModalButtonFull]}
+                textStyle={[ModalButtonText, ModalButtonFullText]}
+                onPress={() => setModalVisible(false)}
+                text={"Non"}
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
       <View style={ActionView}>
-        <Button style={[ButtonActionView, ButtonActionDarkView]}><Image style={ActionImage}
-                                                                        source={require("./cross.png")}/></Button>
-        <Button style={[ButtonActionView, ButtonActionLightView]}>
+        <Button style={[ButtonActionView, ButtonActionDarkView]} onPress={closeGame}>
+          <Image style={ActionImage} source={require("./cross.png")}/>
+        </Button>
+        <Button style={[ButtonActionView, ButtonActionLightView]} onPress={askHelp}>
           <Text style={ActionText}>?</Text>
         </Button>
       </View>
 
-      <View style={ScannerImageView}>
-        <Image style={ScannerImage} source={require("./scanner.png")}/>
-        <View style={ScannerMiddleView}/>
-      </View>
-      <Button style={InventoryView}>
+      <TouchableNativeFeedback onPress={scan}>
+        <View style={ScannerImageView}>
+          <Image style={ScannerImage} source={require("./scanner.png")}/>
+          <View style={ScannerMiddleView}/>
+        </View>
+      </TouchableNativeFeedback>
+      <Button style={InventoryView} onPress={openInventory}>
         <Image source={require("./inventory.png")}/>
       </Button>
     </Screen>
